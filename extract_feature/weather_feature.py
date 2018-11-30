4ebed192b2f1d2194a431d54d14b722b3ff5f1f6
SCUZin <1050087206@qq.com>
2018-11-30 21:50:30 +0800

    commit

--- extract_feature/weather_feature.py
+++ extract_feature/weather_feature.py
@@ -1,4 +1,4 @@
-# 分析天气数据的分布，各个天气下订单量的多少等?取影响较大的区域，而不是整体流量？ 这点还没做
+# 读取每一天的天气数据文件，进行特征提取，得到weather_feature.csv
 
 import pandas as pd
 import numpy as np
@@ -17,22 +17,39 @@ def weather_statistics(file_list, time_interval_para=30):
         df.time = df.time.str.split(':')
         df.time = df.time.map(lambda x: int(x[0]) * 2 + int(x[1]) // time_interval_para)
 
-        # size = df.groupby('time').size()
-        # weather 取众数, groupby('a') 没有 mode()函数
+        size = df.groupby('time').size()
         df_drop_dupli = df.drop_duplicates(['time'])
+
+        # df.loc[(df['weather'] > 4)] = 4   # 用这行代码的话 df_weather_mode 的取众数要出错，我也不知道为什么
+        # weather > 4 的天气全部转为4（下雨）
+        for j in range(len(df)):
+            if df.loc[j, 'weather'] > 4:
+                df.loc[j, 'weather'] = 4
+
+        # weather 取众数, groupby('a') 没有 mode()函数
         df_weather_mode = df.groupby('time')['weather'].agg(lambda x: x.value_counts().index[0]).reset_index()
-        df_weather_of_day_mode = df['weather'].mode()   # 当天的所有天气情况取众数
+
+        # 当天7:00-23:00（14-45时间片，共32个时间片）的所有天气情况取众数
+        # df_weather_of_day_mode = df['weather'].mode()
+        df_weather_of_day_mode = df.loc[(df['time'] >= 14) & (df['time'] <= 45)]['weather'].mode()
         temperature_mean = df.groupby('time')['temperature'].mean()
         pm25_mean = df.groupby('time').mean()['pm2.5']
+
+        # 提取 7:00-23:00（14-45时间片，共32个时间片） temperature 和 pm2.5 的平均值
+        temp_mean_day = df.loc[(df['time'] >= 14) & (df['time'] <= 45)].temperature.mean()
+        pm25_mean_day = df.loc[(df['time'] >= 14) & (df['time'] <= 45)]['pm2.5'].mean()
 
         df_temp = pd.DataFrame({'date': df.loc[0, 'date'],
                                 'time': list(df_drop_dupli.time),
                                 'weather': list(df_weather_mode.weather),
                                 'weather_of_day': df_weather_of_day_mode[0],
                                 'temperature': list(temperature_mean),
-                                'pm2.5': list(pm25_mean)})
+                                'temp_mean_day': temp_mean_day,
+                                'pm2.5': list(pm25_mean),
+                                'pm2.5_mean_day': pm25_mean_day})
 
-        df_temp = df_temp[['date', 'time', 'weather', 'weather_of_day', 'temperature', 'pm2.5']]
+        df_temp = df_temp[['date', 'time', 'weather', 'weather_of_day', 'temperature',
+                           'temp_mean_day', 'pm2.5', 'pm2.5_mean_day']]
         df_temp.set_index(keys=['date', 'time'], inplace=True)
 
         global df_out
@@ -41,8 +58,6 @@ def weather_statistics(file_list, time_interval_para=30):
         else:
             # df_out = df_out.append(df_temp)
             df_out = pd.concat([df_out, df_temp], axis=0)
-
-        print(len(df_out))
 
     dest_path = 'E:\\data\\DiDiData\\data_csv\\weather_data\\weather_feature.csv'
     df_out.to_csv(dest_path)
